576b00dd094a66f9217df2349153b91f10975a19
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-05 19:50:20 -0300

    fix categorias

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,11 +1,29 @@
 
-import React, { useState } from "react"
 import { Link } from "react-router-dom"
+
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../service/api.js"
 
 export const Navbar = () => {
 
 
     const [MobileMenu, setMobileMenu] = useState(false)
+    const [categoria, setCategoria] = useState([])
+
+
+
+    useEffect(() => {
+        api
+          .get(`/categoria/`)
+          .then((response) => {
+            console.log(response.data)
+            setCategoria(response.data)
+          })
+          .catch((err) => {
+            console.error("ops! ocorreu um erro" + err);
+          });
+      }, []);
 
 
     return (
@@ -13,21 +31,16 @@ export const Navbar = () => {
             <div className="container d_c_flex">
                 <div className="navlink">
                     <ul className={MobileMenu ? "nav-links-mobile" : "link f_flex capitalize"} onClick={() => setMobileMenu(false)}>
-                        <li>
-                             <Link to="/">Início</Link> 
-                        </li>
-
-                        <li>
-                             <Link to="/escritorio">Escritório</Link> 
-                        </li>
-                        <li>
-                             <Link to="/livraria">Livraria</Link> 
-                        </li>
-
-                        <li>
-                             <Link to="/informatica">Informática</Link> 
-                        </li>
-
+                        {categoria?.map((cat) => {
+                          return(  
+                            <li key={cat.id}>
+                                <Link to={`/categoria/${cat.id}`}>
+                                    {cat.nome}    
+                                </Link>    
+                            </li>
+                            
+                            )
+                        })}
                         
                     </ul>
 
