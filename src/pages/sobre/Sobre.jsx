af677433db1c04a27d2befc91f7f81d4ac8d98da
Priscila Durão <pd.b@yahoo.com>
2022-11-05 20:34:50 -0300

    Css Quem somo

--- src/pages/sobre/Sobre.jsx
+++ src/pages/sobre/Sobre.jsx
@@ -3,6 +3,11 @@ import { Footer } from '../../components/Footer/Footer.jsx';
 import { Header } from "../../components/header/Header.jsx"
 import "./Sobre.css"
 import fotoJo from "../../assets/jo.png"
+import fotogab from "../../assets/gab.png"
+import fotokauan from "../../assets/kauan.jpg"
+import fotopri from "../../assets/priscila.jpg"
+import fotoma from "../../assets/ma.jpeg"
+import fotoro from "../../assets/ro.jpeg"
 
 
 export const Sobre = () => {
@@ -11,56 +16,42 @@ export const Sobre = () => {
             <Header />
             <h1>PAGINA QUEM SOMOS</h1>
             <div className="bg-light py-5">
-            <div className="container py-5">
-                <div className="row text-center justify-content-between">
-                    <div className="col-xl-3 col-sm-6 mb-5 time">
-                        <div class="bg-white rounded shadow-sm py-5 px-4 time-div">
-                            <img src={fotoJo} alt="Jonathan" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm img-time" />
-                            <h5>Jonathan Marques</h5>
+            <div className="py-5">
+                <div className="pai">
+                        <div class="mini">
+                            <img src={fotoJo} alt="Jonathan" className="img" />
+                            <div className="h5">Jonathan Marques</div>
                             <span className="small"> Bla bla bla, eu sou Jonathan.</span>
                         </div>
-                    </div>
-                    <div className="col-xl-3 col-sm-6 mb-5 time">
-                        <div class="bg-white rounded shadow-sm py-5 px-4 time-div">
-                            <img src="" alt="" />
-                            <h5>Gabriel Martins</h5>
-                            <span className="small"></span>
+                        <div class="mini">
+                            <img src={fotogab} alt="Gabriel" className="img" />
+                            <div className="h5">Gabriel Martins</div>
+                            <span className="small">aaaaaaaaaa</span>
                         </div>
-                    </div>
-                    <div className="col-xl-3 col-sm-6 mb-5 time">
-                        <div class="bg-white rounded shadow-sm py-5 px-4 time-div">
-                            <img src="" alt="" />
-                            <h5>Kauan Oliveira</h5>
-                            <span className="small"></span>
+                        <div class="mini">
+                            <img src={fotokauan} alt="Kauan" className="img" />
+                            <div className="h5">Kauan Oliveira</div>
+                            <span className="small">aaaaaaaaaaaaaaaaaaa</span>
                         </div>
-                    </div>
-                </div>
-                <div className="row text-center justify-content-between">
-                    <div className="col-xl-3 col-sm-6 mb-5 time">
-                        <div class="bg-white rounded shadow-sm py-5 px-4 time-div">
-                            <img src="" alt="" />
+                        <div class="mini">
+                            <img src={fotopri} alt="Priscila" className="img"  />
                             <h5>Priscila Durão</h5>
-                            <span className="small"></span>
+                            <span className="small">aaaaaaaaaaaaaa</span>
                         </div>
-                    </div>
-                    <div className="col-xl-3 col-sm-6 mb-5 time">
-                        <div class="bg-white rounded shadow-sm py-5 px-4 time-div">
-                            <img src="" alt="" />
-                            <h5>Maria Eduarda</h5>
-                            <span className="small"></span>
+                        <div class="mini">
+                            <img src= {fotoma} alt="Maria" className="img" />
+                            <div className="h5">Maria Eduarda</div>
+                            <span className="small">aaaaaaaaaaaa</span>
                         </div>
-                    </div>
-                    <div className="col-xl-3 col-sm-6 mb-5 time">
-                        <div class="bg-white rounded shadow-sm py-5 px-4 time-div">
-                            <img src="" alt="" />
-                            <h5>Roberto o que?</h5>
-                            <span className="small"></span>
+                        <div class="mini">
+                            <img src={fotoro} alt="Roberto" className="img"  />
+                            <div className="h5">Roberto de Andrade</div>
+                            <span className="small">aaaaaaaaaaaa</span>
                         </div>
-                    </div>
                 </div>
             </div>
             </div>
-
+            
             <Footer />
         </>
     )
