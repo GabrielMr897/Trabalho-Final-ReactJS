8a965f179271b2bedb9ff31b9771ad4207ba93e3
Priscila Durão <pd.b@yahoo.com>
2022-11-04 22:50:58 -0300

    Logo / Cards

--- src/pages/home/Produtos.jsx
+++ src/pages/home/Produtos.jsx
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardTitle,
+  Container,
+} from "../../components/Cards/Card.jsx";
+import api from "../../service/api.js";
 
-import api from "../../service/api.js"
+export const Produtos = ({ Cardbox }) => {
+  const [produtos, setProduto] = useState([]);
+  useEffect(() => {
+    api
+      .get("/produto")
+      .then((response) => setProduto(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
 
-
-
-export const Produtos = () => {
-    const [produtos, setProduto] = useState([]);
-    useEffect(() => {
-      api
-        .get("/produto")
-        .then((response) => setProduto(response.data))
-        .catch((err) => {
-          console.error("ops! ocorreu um erro" + err);
-        });
-    }, []);
-
-
-
-
-    return (
-        <>
-        <h1>TODOS OS PRODUTOS</h1>
-            {
-            produtos?.map((produto) => {
-                return(
-                <> 
-                
-                <div key={produto.id}>
-                    <h2>{produto.nome}</h2>
-                    <h3>{produto.descricao}</h3>
-                    <p>{produto.valor}</p>
-                </div>
-                </>)
-            })}
-        </>
-        
-    )
-}
+  return (
+    <>
+      <h1>TODOS OS PRODUTOS</h1>
+      <Container>
+        {produtos?.map((produto) => {
+          return (
+            <Card key={produto.id}>
+              <CardImg src={produto.foto} />
+              <CardTitle>{produto.nome}</CardTitle>
+              <CardText>R$ {produto.valor}</CardText>
+              <CardTitle></CardTitle>
+            </Card>
+          );
+        })}
+      </Container>
+    </>
+  );
+};
