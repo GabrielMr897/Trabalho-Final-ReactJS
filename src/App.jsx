ea976a78ca90e2fbfda62c0429eb8c6f837342da
JotaMarquesb <jonathanmarquesb@hotmail.com>
2022-11-05 17:04:06 -0300

    incluiu banner

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contato" element={<Contato />} />
-          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/quemsomos" element={<Sobre />} />
           <Route path="/descricao/:id" element={<Descricao />} />
         </Routes>
       </Router>
