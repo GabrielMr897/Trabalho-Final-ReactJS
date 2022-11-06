4b8756e61f26e5a3e3a78bdde853e7c7a2b1730e
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-06 13:57:39 -0300

    P branch (#32)
    * Atualização cards
    
    * G branch (#19)
    
    * correção
    
    * page detalhe
    
    * corrigindo import
    
    * deletando pastas
    
    * Css Quem somo
    
    * Descrição estilização
    
    * Botão
    
    Co-authored-by: Priscila Durão <pd.b@yahoo.com>

--- src/pages/DescricaoProduto/descricao.jsx
+++ src/pages/DescricaoProduto/descricao.jsx
@@ -14,6 +14,7 @@ import {
   CardDesc
 } from "../../components/Cards/Card.jsx";
 import { autoprefixer } from "fontawesome";
+import { Button } from "../../components/Button/botao.jsx"
 
 export const Descricao = () => {
   const [produtos, setProdutos] = useState([]);
@@ -40,13 +41,28 @@ export const Descricao = () => {
         <CardImg src={produtos.fotoLink}></CardImg>
         <CardDesc>{produtos.descricao}</CardDesc>
        <CardText>R$ {produtos.valor}</CardText>
+
         <button onClick={()=>addItemCart(
+
           produtos.id,
           produtos.fotoLink,
           produtos.nome,
           produtos.valor
+
+        )}  backgroundColor="RGB(98, 210, 162)"
+        name="Adicionar ao carrinho"
+        fontSize={30}
+        width="15rem"
+        height="2rem"
+        colorText="#ffffff"
+        borderradius="6px"
+        textalign="center"
+        />
+ 
+
         )}>adcionar ao carrinho</button>
      
+
         </Fixo>
      
         </Container>
