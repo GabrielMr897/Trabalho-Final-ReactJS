ce76f6ac0681b37c548b64af36759192dfdba520
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-07 17:09:30 -0300

    painel

---
+++ src/pages/admin/Painel.jsx
@@ -0,0 +1,15 @@
+import { Card } from "../../components/Cards/Card"
+
+
+export const PainelAdmin = () => {
+    
+    return (
+            
+        <ul>
+            <li>Categoria</li>
+            <li>Produto</li>
+            <li>Cliente</li>
+            <li>Funcionario</li>
+        </ul>
+    )
+}
