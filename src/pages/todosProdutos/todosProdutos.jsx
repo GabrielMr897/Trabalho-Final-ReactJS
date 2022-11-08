96dce88c422a1349c950ae75a0794d84e6a44f81
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-08 18:42:21 -0300

    estilizando pagina

---
+++ src/pages/todosProdutos/todosProdutos.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardTitle,
+  Container,
+} from "../../components/Cards/Card.jsx";
+import api from "../../service/api.js";
+
+
+import { Link } from "react-router-dom"
+
+export const TodosProdutos = () => {
+  const [produtos, setProduto] = useState([]);
+  useEffect(() => {
+    api
+    .get("/produto")
+    .then((response) => setProduto(response.data))
+    .catch((err) => {
+      console.error("ops! ocorreu um erro" + err);
+    });
+  }, []);
+  
+  
+  return (
+    <>
+      <h1>TODOS OS PRODUTOS</h1>
+      <Container>
+    
+        {produtos?.map((produto) => {
+              return (
+                <Card key={produto.id}>
+                <Link to={`/descricao/${produto.id}`}>
+                  <CardImg src={produto.fotoLink}></CardImg>
+                  <CardTitle>{produto.nome}</CardTitle>
+                  <CardText>R$ {produto.valor}</CardText>
+                  
+                  </Link>
+                </Card>
+              );
+            
+        })}
+      </Container>
+    </>
+  );
+};
